02cfcda018daf0ccca3c17d5fcc020a646fd7ecd
dashingcoderrohan123 <anandrao23046@gmail.com>
2024-10-30 17:33:53 +0530

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,7 @@
+print("Welcome to the world of programming")
+print(34)#numbers are never written quotation
+print("Rohan",12) #everything written in quotation is string
+print("Sudeeshna",17)
+print("I stay in hyderabad \n")#\n is for next line
+print("i am a student\n")
+print("Bye",end=":)")
---
+++ lesson3.py
@@ -0,0 +1,8 @@
+rohan="student"
+power=99999
+print(rohan)
+print(power)
+name=input("Enter your name: ")
+print(name)
+age=input("enter your age: ")
+print(age)
